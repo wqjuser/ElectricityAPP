4a31d47d291cb65ab580ee7724a14af151f9b400
WQJ <wqjuser@gmail.com>
2018-09-12 20:06:46 +0800

    增加kotlin相关扩展

--- UserCenter/src/main/java/com/wqj/usercenter/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/wqj/usercenter/ui/activity/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.wqj.usercenter.R
 import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.toast
 
 class RegisterActivity : AppCompatActivity() {
@@ -13,6 +14,7 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
         mBtnRegister.setOnClickListener {
             toast("测试")
+            intentFor<RegisterActivity>("id" to 5)
         }
     }
 }
