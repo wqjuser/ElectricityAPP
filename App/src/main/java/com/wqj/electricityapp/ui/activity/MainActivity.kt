66e5ff347e859983c4329176e7be9abaa39a0361
WQJ <wqjuser@gmail.com>
2018-09-12 11:34:52 +0800

    首次提交

---
+++ App/src/main/java/com/wqj/electricityapp/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.wqj.electricityapp.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.wqj.electricityapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
